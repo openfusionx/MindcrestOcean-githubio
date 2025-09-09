c02775db15d9f412a82fcf081d7cd6dc29904fa9
Suyuchen <1258539920@qq.com>
2025-09-09 14:22:04 +0800

    修改路径

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,18 +25,18 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Nexus Fabric',
+  title: 'Mindcrest Ocean',
   tagline: '致力于快速实现部署、管理、监控以及自动化运维大数据云原生平台，帮助您快速构建起稳定、高效、可弹性伸缩的大数据云原生平台',
-  url: 'https://datasophon.github.io',
-  baseUrl: '/datasophon-website/',
+  url: 'https://mindcrestocean.github.io',
+  baseUrl: '/mindcrestocean-website/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/brand.svg',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'datasophon', // Usually your GitHub org/user name.
-  projectName: 'datasophon-website', // Usually your repo name.
+  organizationName: 'mindcrestocean', // Usually your GitHub org/user name.
+  projectName: 'mindcrestocean-website', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -71,7 +71,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/datasophon/datasophon-website/',
+            'https://github.com/mindcrestocean/mindcrestocean-website/',
         },
         // 移除博客配置
         // blog: false,
@@ -115,7 +115,7 @@ const config = {
                 value: '<hr style="margin: 0.3rem 0;">',
               },
               {
-                href: 'https://github.com/datasophon/datasophon-website/',
+                href: 'https://github.com/mindcrestocean/mindcrestocean-website/',
                 label: 'Help Us Translate',
               },
             ],
@@ -129,7 +129,7 @@ const config = {
           // 移除博客链接
           // {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/datasophon/datasophon',
+            href: 'https://github.com/mindcrestocean/mindcrestocean',
             label: 'GitHub',
             position: 'left',
           },
@@ -156,7 +156,7 @@ const config = {
               // },
               {
                 label: 'Github Discussion',
-                href: 'https://github.com/datasophon/datasophon',
+                href: 'https://github.com/mindcrestocean/mindcrestocean',
               },
               // {
               //   label: 'Twitter',
@@ -174,7 +174,7 @@ const config = {
               // },
               {
                 label: 'GitHub Issues',
-                href: 'https://github.com/datasophon/datasophon/issues',
+                href: 'https://github.com/mindcrestocean/mindcrestocean/issues',
               },
             ],
           },
